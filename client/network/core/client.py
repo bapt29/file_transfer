9ada984a9ce101c5d97c368d829a40780e774fe4
user <baptiste.cabiten@gmail.com>
2018-05-24 22:01:27 +0200

    set a timeout and modified connect and added disconnect method

--- client/network/core/client.py
+++ client/network/core/client.py
@@ -8,17 +8,16 @@ class Client:
         self.port = port
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(5)
 
     def connect(self):
-        try:
-            self.socket.connect((self.ip_address, self.port))
-        except socket.error as e:
-            return e
-
-        return None
+        self.socket.connect((self.ip_address, self.port))
 
     def send(self, data: bytes) -> None:
         self.socket.send(data)
 
     def receive(self) -> bytes:
         return self.socket.recv(1024)
+
+    def disconnect(self):
+        self.socket.close()

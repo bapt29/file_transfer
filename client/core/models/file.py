72a5cdba78b3a6a2d014172c443426da07ced8f7
user <baptiste.cabiten@gmail.com>
2018-05-27 21:34:45 +0200

    added few attributes

--- client/core/models/file.py
+++ client/core/models/file.py
@@ -11,17 +11,21 @@ class File:
 
         self.path = path
         self.name = None
-        self.file = None
+        self.file_object = None
         self.size = 0
         self.checksum = None
+        self.chunk_size = chunk_size * 10**3  # Chunk in Ko
 
-        self.chunk_size = chunk_size * 1000  # Chunk in Ko
         self.current_chunk = 0
+        self.current_chunk_size = None
         self.current_chunk_data = None
         self.current_chunk_checksum = None
 
+        self.total_bytes_sent = 0
+        self.last_chunk_sent_time = None
+
     def is_opened(self) -> bool:
-        if self.file is None:
+        if self.file_object is None:
             return False
 
         return True

25612ffdcd41b2f6a25fa991c279c5ed5a039565
user <baptiste.cabiten@gmail.com>
2018-05-27 21:34:13 +0200

    added update_last_chunk_time method

--- client/core/controllers/file_controller.py
+++ client/core/controllers/file_controller.py
@@ -1,5 +1,6 @@
 import os
 import hashlib
+from datetime import datetime
 
 from client.core.models.file import File
 from client.errors.file_errors import *
@@ -19,14 +20,14 @@ class FileController:
 
     def is_file_already_in_list(self, path: str) -> bool:
         for file in self.files_list:
-            if file.path == path:  # File already in the list
+            if file.path == path:
                 return True
 
         return False
 
     @staticmethod
-    def get_name(file: File) -> str:
-        return file.path.split("/")[-1]
+    def set_name(file: File) -> None:
+        file.name = file.path.split("/")[-1]
 
     @staticmethod
     def set_file_size(file: File) -> None:
@@ -42,7 +43,7 @@ class FileController:
         if file.is_opened() and file.current_chunk == 0:
             checksum_function = hashlib.md5()
 
-            for chunk in iter(lambda: file.file.read(file.chunk_size), b""):
+            for chunk in iter(lambda: file.file_object.read(file.chunk_size), b""):
                 checksum_function.update(chunk)
 
             file.checksum = checksum_function.hexdigest()
@@ -57,19 +58,24 @@ class FileController:
             file.current_chunk_checksum = checksum_function.hexdigest()
 
     @staticmethod
+    def update_last_chunk_time(file: File) -> None:
+        file.last_chunk_sent_time = datetime.now()
+
+    @staticmethod
     def open(file: File) -> None:
         if file.is_opened():
             return None
 
-        file.file = open(file.path, "rb")
+        file.file_object = open(file.path, "rb")
 
+        FileController.set_name(file)
         FileController.set_file_size(file)
         FileController.set_file_checksum(file)
 
     @staticmethod
     def close(file: File) -> None:
         if file.is_opened():
-            file.file.close()
+            file.file_object.close()
 
     @staticmethod
     def read(file: File) -> None:
@@ -79,16 +85,20 @@ class FileController:
         if file.current_chunk == -1:
             raise EndOfFile
 
-        file.current_chunk_data = file.file.read(file.chunk_size)
+        file.current_chunk_data = file.file_object.read(file.chunk_size)
 
         if file.current_chunk_data == b"":
             file.current_chunk = -1
             raise EndOfFile
 
+        current_chunk_size = len(file.current_chunk_data)
+
         FileController.set_chunk_checksum(file)
+        file.total_bytes_sent += current_chunk_size
+        file.current_chunk_size = current_chunk_size
         file.current_chunk += 1
 
     @staticmethod
     def go_to_byte(file: File, byte_number: int) -> None:
         if file.is_opened() and byte_number <= file.size:
-            file.file.fseek(byte_number)
+            file.file_object.seek(byte_number)

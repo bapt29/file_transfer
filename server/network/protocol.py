49245baeb51fce4fb14cc24d2709b9536484676b
bapt29 <baptiste.cabiten@gmail.com>
2018-05-28 18:04:09 +0200

    added methods

--- server/network/protocol.py
+++ server/network/protocol.py
@@ -4,4 +4,13 @@ from typing import Tuple
 class Protocol:
 
     @staticmethod
-    def process_packet(data: bytearray) -> Tuple[int, bytearray]:
+    def confirmation_packet(response: bool) -> bytearray:
+        pass
+
+    @staticmethod
+    def file_chunk_integrity_confirmation(response: bool) -> bytearray:
+        pass
+
+    @staticmethod
+    def file_integrity_confirmation(response: bool) -> bytearray:
+        pass

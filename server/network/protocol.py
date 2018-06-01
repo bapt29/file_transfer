e4fc8d3b58b07069eb83e1ef3f7167bd7b816ce6
user <baptiste.cabiten@gmail.com>
2018-06-01 09:37:39 +0200

    implemented protocol

--- server/network/protocol.py
+++ server/network/protocol.py
@@ -1,16 +1,32 @@
 from typing import Tuple
+import struct
 
 
 class Protocol:
 
     @staticmethod
     def confirmation_packet(response: bool) -> bytearray:
-        pass
+        data = bytearray()
+        data.append(0x02)
+
+        data.extend(bytearray(struct.pack("?", response)))
+
+        return data
 
     @staticmethod
     def file_chunk_integrity_confirmation(response: bool) -> bytearray:
-        pass
+        data = bytearray()
+        data.append(0x03)
+
+        data.extend(bytearray(struct.pack("?", response)))
+
+        return data
 
     @staticmethod
     def file_integrity_confirmation(response: bool) -> bytearray:
-        pass
+        data = bytearray()
+        data.append(0x04)
+
+        data.extend(bytearray(struct.pack("?", response)))
+
+        return data
